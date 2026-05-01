d0e0fb951dd2e6b0f3c270611c08e0ed9826ea3e
VandalxGod <sumit.tutu2506@gmail.com>
2026-05-01 06:32:51 +0530

    Adding Backend url in Frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://team-task-manager-sdo8.onrender.com",
 });
 
 // Attach token automatically
